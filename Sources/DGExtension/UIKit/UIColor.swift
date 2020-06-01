945af96cdd14ba398fab795ff08a06432e318e0d
Xiao Jin <debugeek@gmail.com>
2020-06-01 19:22:06 +0800

    Update function

--- Sources/DGExtension/UIKit/UIColor.swift
+++ Sources/DGExtension/UIKit/UIColor.swift
@@ -10,11 +10,20 @@ import UIKit
 
 public extension UIColor {
     
-    @available(iOS 10.0, *)
     func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
+        if #available(iOS 10.0, *) {
+            return UIGraphicsImageRenderer(size: size).image { rendererContext in
+                self.setFill()
+                rendererContext.fill(CGRect(origin: .zero, size: size))
+            }
+        } else {
+            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            UIGraphicsBeginImageContextWithOptions(size, false, 0)
+            setFill()
+            UIRectFill(rect)
+            let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            return image
         }
     }
     
